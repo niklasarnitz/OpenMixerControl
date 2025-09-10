dfb01af133cdf9fbaba8b2770802bcb58157656e
Dr. Christian Nöding <christian.noeding@uni-kassel.de>
2025-09-10 21:51:12 +0200

    Added missing header

---
+++ src/hal.h
@@ -0,0 +1,11 @@
+#ifndef HAL_H_
+#define HAL_H_
+
+#include "x32ctrl.h"
+
+void halSetGain(uint8_t group, uint8_t channel, float gain);
+void halSetPhantomPower(uint8_t group, uint8_t channel, bool active);
+void halSetVolume(uint8_t channel, float volume);
+void halSetBalance(uint8_t channel, uint8_t balance);
+
+#endif
