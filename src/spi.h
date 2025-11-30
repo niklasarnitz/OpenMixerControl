eebbbf51197fb4f3a1a5d680ae88915801ac3f63
Alexander Schulz <code@schulzalex.de>
2025-12-01 00:00:58 +0100

    SPI: DSP speed lowered to 1MHz, DSP2 communicates now

--- src/spi.h
+++ src/spi.h
@@ -17,7 +17,7 @@
 #define SPI_FPGA_XILINX_SPEED_HZ 10000000 // 10 MHz for the FPGA, seems to be fine
 #define SPI_FPGA_LATTICE_SPEED_HZ (8000 * 1024) // ~8MHz
 #define SPI_DSP_CONFIG_SPEED_HZ 250000 // 250 kHz for the DSP. Higher and we get problems
-#define SPI_DSP_SPEED_HZ 2000000 // 16 MHz is the regular SPI-clock on the original X32
+#define SPI_DSP_SPEED_HZ 1000000 // 16 MHz is the regular SPI-clock on the original X32
 
 // defines for FPGA-configuration via SPI
 #define PROG_B_GPIO_OFFSET      30
